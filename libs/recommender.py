88339442af9233260675fadec8061ecb05a8b03a
codermango <codermango@gmail.com>
2015-04-17 16:34:02 +0200

    fix bug, still have bugs

--- libs/recommender.py
+++ libs/recommender.py
@@ -20,7 +20,21 @@ def generate_result(genre_cos_sim_dic, mawid_cos_sim_dic, releaseyear_cos_sim_di
     releaseyear_cos_sim_counter = Counter(releaseyear_cos_sim_dic)
 
     combined_cos_sim_counter = genre_cos_sim_counter + mawid_cos_sim_counter + releaseyear_cos_sim_counter
-    
+    print "///////////////////", genre_cos_sim_counter.most_common(10)
+
+    print genre_cos_sim_counter["tt0114369"], genre_cos_sim_counter["tt0111893"], genre_cos_sim_counter["tt0105695"], genre_cos_sim_counter["tt0100813"], genre_cos_sim_counter["tt0120584"]
+    print mawid_cos_sim_counter["tt0114369"], mawid_cos_sim_counter["tt0111893"], mawid_cos_sim_counter["tt0105695"], mawid_cos_sim_counter["tt0100813"], mawid_cos_sim_counter["tt0120584"]
+    print releaseyear_cos_sim_counter["tt0114369"], releaseyear_cos_sim_counter["tt0111893"], releaseyear_cos_sim_counter["tt0105695"], releaseyear_cos_sim_counter["tt0100813"], releaseyear_cos_sim_counter["tt0120584"]
+
+
+
+    imdbrating_file = open(os.path.split(os.path.realpath(__file__))[0] + "/imdbrating.json")
+    imdbrating_dict = json.loads(imdbrating_file.readline())
+    for k, v in imdbrating_dict.items():
+        combined_cos_sim_counter[k] *= imdbrating_dict[k]
+
+
+
     # for key in user_liked_movie_id_list:    
     #     del combined_cos_sim_counter[key]
     #     del genre_cos_sim_counter[key]
@@ -31,7 +45,7 @@ def generate_result(genre_cos_sim_dic, mawid_cos_sim_dic, releaseyear_cos_sim_di
     final_mawid_recommended_movies = mawid_cos_sim_counter.most_common(num_of_recommended_movies)
     final_releaseyear_recommended_movies = releaseyear_cos_sim_counter.most_common(num_of_recommended_movies)
 
-    # print 'final_co_recommended_movies:', final_co_recommended_movies
+    print 'final_co_recommended_movies:', final_co_recommended_movies
     # print final_genre_recommended_movies
     # print final_mawid_recommended_movies
 
@@ -49,10 +63,11 @@ def recommend(user_liked_movie_id_list, recommend_method="all"):
 
     # 以下可分别得到根据genre和mawid推荐出的结果，均为（movied_id: cos_sim_value）这种的字典
     genre_cos_sim_dic = genre_recommender.recommend(user_liked_movie_id_list)
+    print "+++++++", sum(genre_cos_sim_dic.values())
     mawid_cos_sim_dic = mawid_recommender.recommend(user_liked_movie_id_list)
     releaseyear_cos_sim_dic = releaseyear_recommender.recommend(user_liked_movie_id_list)
 
-    num_of_recommended_movies = 10
+    num_of_recommended_movies = 30
     result = generate_result(genre_cos_sim_dic, mawid_cos_sim_dic, releaseyear_cos_sim_dic, num_of_recommended_movies, user_liked_movie_id_list)
     print result
     return dict(result[recommend_method]).keys()
@@ -61,21 +76,15 @@ def recommend(user_liked_movie_id_list, recommend_method="all"):
 
 ###########################################################################
 ###########################################################################
-###########################################################################
-###########################################################################
-###########################################################################
 
 
 
 
-# my_liked_movie_id_file = open("mark_liked_movie_id.txt")
-# 把文件中的id放入list
-user_liked_movie_id_list = ["tt0348124","tt0401398","tt0486761","tt0181196","tt0389074","tt0279967","tt0969647","tt0097757","tt0103639"]
-# # # for line_of_my_liked_movie_list in my_liked_movie_id_file:
-# # #     user_liked_movie_id_list.append(line_of_my_liked_movie_list.strip())
-# # # print user_liked_movie_id_list
 
-#user_liked_movie_id_list = ["tt1229821","tt0401398"]
+
+
+user_liked_movie_id_list = ["tt0133093","tt0137523","tt0468569","tt0172495","tt0114369","tt1375666","tt0361862","tt0482571","tt0268978","tt0110322"]
+
 id_list = recommend(user_liked_movie_id_list)
 
 print "final recommend:", id_list
