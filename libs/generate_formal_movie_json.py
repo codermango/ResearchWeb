8f9b6138b8ee5ef0a664d23c1aca8fe8a303d5b6
codermango <codermango@gmail.com>
2015-04-20 15:46:29 +0200

    add main actors and directors to data source

--- libs/generate_formal_movie_json.py
+++ libs/generate_formal_movie_json.py
@@ -8,9 +8,9 @@ import os
     {
         "imdbId": "ttxxxxxx",
         "genres": [xxx, xxx, ...],
-        "mawid": [xxx, xxx, ...],
+        "imdbMainactors": [xxx, xxx, ...],
         "releaseYear": 2014,
-        "directors": [xxx, xxx],
+        "imdbDirectors": [xxx, xxx],
         "imdbRating": 0
     }
 '''
@@ -21,13 +21,17 @@ tagdb_after_neaten_genres_file = open("tagdb_after_neaten_genres.json")
 movieid_with_mawid_file = open("movie_id_with_mawid.json")
 movieid_with_mawid_dict = json.loads(movieid_with_mawid_file.readline())
 
+imdbmainactors_file = open("imdbmainactors.json")
+imdbmainactors_dict = json.loads(imdbmainactors_file.readline())
+
 movieid_with_releaseyear_file = open("movieid_with_releaseyear.json")
 movieid_with_releaseyear_dict = json.loads(movieid_with_releaseyear_file.readline())
 
-imdbrating_file = open("../info/imdbrating.json")
+imdbrating_file = open("imdbrating.json")
 imdbrating_dict = json.loads(imdbrating_file.readline())
 
-
+imdbdirectors_file = open("imdbdirectors.json")
+imdbdirectors_dict = json.loads(imdbdirectors_file.readline())
 
 
 all_movies_file = open("all_movies.dat", "w")
@@ -42,11 +46,13 @@ for line in tagdb_after_neaten_genres_file:
     new_movie_format = dict()
     new_movie_format["imdbId"] = imdbid
     new_movie_format["genres"] = genres
-    new_movie_format["mawid"] = movieid_with_mawid_dict[imdbid]
     new_movie_format["releaseYear"] = int(movieid_with_releaseyear_dict[imdbid])
-    new_movie_format["directors"] = []
+    
+    
     try:
         new_movie_format["imdbRating"] = imdbrating_dict[imdbid]
+        new_movie_format["imdbDirectors"] = imdbdirectors_dict[imdbid]
+        new_movie_format["imdbMainactors"] = imdbmainactors_dict[imdbid]
     except KeyError:
         print imdbid
         continue
@@ -61,3 +67,5 @@ tagdb_after_neaten_genres_file.close()
 movieid_with_mawid_file.close()
 movieid_with_releaseyear_file.close()
 imdbrating_file.close()
+imdbdirectors_file.close()
+imdbmainactors_file.close()
