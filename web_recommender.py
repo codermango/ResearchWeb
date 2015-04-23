906f002268a7ba4251e201a7799ad05ab1ee18c8
codermango <codermango@gmail.com>
2015-04-23 16:29:51 +0200

    adjust web

--- web_recommender.py
+++ web_recommender.py
@@ -1,8 +1,8 @@
 import sys
 import os
 sys.path.append(".")
-from flask import Flask, url_for, render_template, request
-import libs.recommender 
+from flask import Flask, url_for, render_template, request, Response
+import recommender_libs.recommender 
 
 app = Flask(__name__)
 
@@ -20,11 +20,24 @@ def web_recommender_result():
         ids_list = ids.split(",")
         trimed_ids_list = map(lambda x: x.strip(), ids_list)
 
-        genre_recommended_ids = libs.recommender.recommend(trimed_ids_list, "genre")
-        mawid_recommended_ids = libs.recommender.recommend(trimed_ids_list, "mawid")
-        all_recommended_ids = libs.recommender.recommend(trimed_ids_list, "all")
+        genre_recommended_ids = recommender_libs.recommender.recommend(trimed_ids_list, "genre")
+        actor_recommended_ids = recommender_libs.recommender.recommend(trimed_ids_list, "actor")
+        director_recommended_ids = recommender_libs.recommender.recommend(trimed_ids_list, "director")
+        all_recommended_ids = recommender_libs.recommender.recommend(trimed_ids_list, "all")
 
-        return render_template("web_recommender.html", liked_ids=trimed_ids_list, genre_recommended_ids=genre_recommended_ids, mawid_recommended_ids=mawid_recommended_ids, all_recommended_ids=all_recommended_ids)
+        
+
+        #return Response(stream_template("web_recommender.html", liked_ids=trimed_ids_list, genre_recommended_ids=genre_recommended_ids, mawid_recommended_ids=mawid_recommended_ids, all_recommended_ids=all_recommended_ids))
+        return render_template("web_recommender.html", liked_ids=trimed_ids_list, genre_recommended_ids=genre_recommended_ids, actor_recommended_ids=actor_recommended_ids, director_recommended_ids=director_recommended_ids, all_recommended_ids=all_recommended_ids)
+
+
+def stream_template(template_name, **context):
+    app.update_template_context(context)
+    t = app.jinja_env.get_template(template_name)
+    rv = t.stream(context)
+    rv.enable_buffering(5)
+    return rv
+
 
 
 
